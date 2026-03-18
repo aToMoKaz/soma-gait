e92598fe76a22289e5e3f5fc9c13e277162b4140
aToMoKaz <abe.tomokazu.w6@f.mail.nagoya-u.ac.jp>
2026-03-18 18:01:28 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// SOMA Service Worker v1.0
+const CACHE = 'soma-v1';
+const ASSETS = [
+  './soma_camera.html',
+  './patient_report.html',
+  './research_report.html',
+  './manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
